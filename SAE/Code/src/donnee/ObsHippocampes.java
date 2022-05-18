c7501c4e9e2c5c7e380f3316e49a3f1401b50277
SadPeanut <slanistas29@gmail.com>
2022-05-18 14:38:11 +0200

    setters

--- SAE/Code/src/donnee/ObsHippocampes.java
+++ SAE/Code/src/donnee/ObsHippocampes.java
@@ -74,4 +74,60 @@ public class ObsHippocampes extends Observation {
 	public boolean getEstGestant() {
 		return this.estGestant;
 	}
+
+	/**
+	 * Définit l'espece de ObsHippocampe.
+	 * @param espece espece à définir
+	 */
+	public void setEspece(EspeceHippocampes espece){
+		if(espece!=null){
+			this.espece = espece;
+		}else{
+			System.err.println("setEspece : espece ne peut pas être null");
+		}
+	}
+
+	/**
+	 * Définit le typePeche de ObsHippocampe.
+	 * @param espece typePeche à définir
+	 */
+	public void setTypePeche(Peche typePeche){
+		if(typePeche!=null){
+			this.typePeche = typePeche;
+		}else{
+			System.err.println("setTypePeche : espece ne peut pas être null");
+		}
+	}
+
+	/**
+	 * Définit la taille de ObsHippocampe.
+	 * @param espece taille à définir
+	 */
+	public void setTaille(double taille){
+		if(taille>0){
+			this.taille = taille;
+		}else{
+			System.err.println("setTaille : taille ne peut pas être <0");
+		}
+	}
+
+	/**
+	 * Définit le sexe de ObsHippocampe.
+	 * @param espece sexe à définir
+	 */
+	public void setSexe(Sexe sexe){
+		if(sexe!=null){
+			this.sexe = sexe;
+		}else{
+			System.err.println("setSexe : sexe ne peut pas être null");
+		}
+	}
+
+	/**
+	 * Définit le booleen estGestant de ObsHippocampe.
+	 * @param espece booleen estGestant à définir
+	 */
+	public void setEstGestant(boolean estGestant){
+		this.estGestant = estGestant;
+	}
 }

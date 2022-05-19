2af19b90ac851f4e0ceba52c5e738e4bc001376b
SadPeanut <slanistas29@gmail.com>
2022-05-19 11:44:32 +0200

    cum

--- SAE/Code/src/donnee/IObs.java
+++ SAE/Code/src/donnee/IObs.java
@@ -5,30 +5,30 @@ import java.sql.Time;
 public interface IObs <T>{
 
 	/**
-	 * ajoute une observation à la liste des observations de la chouette
+	 * Ajoute une observation à la liste des observations de la chouette.
 	 * @param obs l'observation à ajouter
 	 */
 	void ajouteUneObs(T obs);
 
 	/**
-	 * ajoute une liste d'observations à la liste des observations de la chouette
+	 * Ajoute une liste d'observations à la liste des observations de la chouette.
 	 * @param obs la liste d'observations à ajouter
 	 */
 	void ajoutePlsObs(ArrayList<T> obs);
 
 	/**
-	 * vide la liste des observations de la chouette
+	 * Vide la liste des observations de la chouette.
 	 */
 	void videObs();
 
 	/**
-	 * retire une observation de la liste des observations de la chouette
+	 * Retire une observation de la liste des observations de la chouette.
 	 * @param idObs l'id de l'observation à retirer
 	 */
 	boolean retireObs(int idObs);
 
 	/**
-	 * retourne le nombre d'observations de la chouette
+	 * Retourne le nombre d'observations de la chouette.
 	 * @return true si l'obs à bien été retirer
 	 */
 	int nbObs();

96a2a7727738fc9ca1c0cf6209485348741e8684
enzofcna <enzolbd@gmail.com>
2022-05-22 17:32:14 +0200

    a

--- SAE/Code/src/donnee/Observation.java
+++ SAE/Code/src/donnee/Observation.java
@@ -15,7 +15,7 @@ public abstract class Observation {
 	/**
 	 * la liste des observateurs
 	 */
-	protected Collection<Observateur> lesObservateurs;
+	protected ArrayList<Observateur> lesObservateurs;
 
 	/**
 	 * identifiatn de l'observation
@@ -79,12 +79,14 @@ public abstract class Observation {
 	 * @param o Observateur a ajouter
 	 */
 	public void ajouteObservateur(Observateur o) {
-		if(o != null){
+		if(lesObservateurs.contains(o)){
+			System.err.println("L'observateur existe déjà");
+		}else if(o == null){
+			System.err.println("L'observateur ne doit pas être null");
+		}else{
 			lesObservateurs.add(o);
 		}
-		else{
-			System.err.println("L'observateur ne doit pas être null");
-		}
+		
 	}
 
 	/**
@@ -93,7 +95,7 @@ public abstract class Observation {
 	 */
 	public void retireObservateur(int idObservateur) {
 
-		if(lesObservateurs != null && idObservateur >= 0){
+		if(lesObservateurs != null && idObservateur >= 0 && idObservateur<lesObservateurs.size()){
 			lesObservateurs.remove(idObservateur); 
 		}
 		else{
@@ -102,7 +104,7 @@ public abstract class Observation {
 	}
 
 	/**
-	 * retorune l'espece observee
+	 * retourne l'espece observee
 	 * @return l'espece observee
 	 */
 	public abstract EspeceObservee especeObs();

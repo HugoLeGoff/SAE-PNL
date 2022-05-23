942a902d3d02c420111f6579b3d9f922684dad7b
Zedrum <peron.yannmael@gmail.com>
2022-05-23 08:38:08 +0200

    sd

--- SAE/Code/src/traitement/Sommet.java
+++ SAE/Code/src/traitement/Sommet.java
@@ -21,6 +21,12 @@ public class Sommet {
      */
     private EpseceObservee espece;
 
+    /**
+     * Constructeur de la classe Sommet
+     * @param id identifiant du sommet
+     * @param coordLieu coordonnées du lieu
+     * @param date date de l'observation
+     */
     public Sommet(int id, Lieu coordLieu, Date date) {
         
         if(id < 0) {
@@ -38,13 +44,23 @@ public class Sommet {
         this.date = date;
     }
 
+    /**
+     * Constructeur de la classe Sommet
+     * @param obs observation
+     */
     public Sommet(Observation obs){
-
+            
         if(obs == null) {
             throw new IllegalArgumentException("obs ne peut être null");
         }
+    
     }
 
+    /**
+     * calcule la distance à parir d'un sommet
+     * @param som sommet
+     * @return distance
+     */
     public double calculeDistance(Sommet som) {
 
         return coordLieu.calculeDistance(som.coordLieu);

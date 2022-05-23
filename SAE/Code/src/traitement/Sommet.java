8daa8abb0810d79abf41349a08deaa42a67599ea
Zedrum <peron.yannmael@gmail.com>
2022-05-23 08:26:09 +0200

    fd

--- SAE/Code/src/traitement/Sommet.java
+++ SAE/Code/src/traitement/Sommet.java
@@ -1,3 +1,32 @@
 public class Sommet {
     
+    private int id;
+    private Lieu coordLieu;
+    private Date date;
+    private EpseceObservee espece;
+
+    public Sommet(int id, Lieu coordLieu, Date date) {
+        
+        if(id < 0) {
+            throw new IllegalArgumentException("id doit être positif");
+        }
+        if(coordLieu == null) {
+            throw new IllegalArgumentException("coordLieu ne peut être null");
+        }
+        if(date == null) {
+            throw new IllegalArgumentException("date ne peut être null");
+        }
+
+        this.id = id;
+        this.coordLieu = coordLieu;
+        this.date = date;
+    }
+
+    public Sommet(Observation obs){
+
+        if(obs == null) {
+            throw new IllegalArgumentException("obs ne peut être null");
+        }
+
+    }
 }

91b8c88db6898b4b91aaf6b9ad1d48e398533652
HugoLeGoff <hugo.legoff56@gmail.com>
2022-06-02 10:32:38 +0200

    push

--- SAE/Code/src/data/Database.java
+++ SAE/Code/src/data/Database.java
@@ -1,6 +1,7 @@
+package data;
 import java.util.*;
-import java.swing.*;
 import java.sql.*;
+
 
 public class Database {
     public static void main(String args[])
@@ -8,19 +9,16 @@ public class Database {
     try
     {
       //étape 1: charger la classe de driver
-      Class.forName("com.mysql.jdbc.Driver");
-      //étape 2: créer l'objet de connexion
-      Connection conn = DriverManager.getConnection(
-      "jdbc:mysql://localhost:3306/bd_pnr", "root@localhost", "EnzoLeBodo2003");
+      Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/bd_pnr", "root", "Hugo2002!");
       //étape 3: créer l'objet statement 
-      Statement stmt = conn.createStatement();
+      /*Statement stmt = conn.createStatement();
       ResultSet res = stmt.executeQuery("SELECT * FROM Observateur LIMIT 10");
       //étape 4: exécuter la requête
       while(res.next())
         System.out.println(res.getInt(1)+"  "+res.getString(2)
         +"  "+res.getString(3));
-      //étape 5: fermez l'objet de connexion
-      conn.close();
+      //étape 5: fermez l'objet de connexion*/
+      c.close();
     }
     catch(Exception e){ 
       System.out.println(e);

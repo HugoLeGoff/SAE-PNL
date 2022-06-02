f6f283b27b713c5ec76ae99a1405150cc392d5a0
Zedrum <peron.yannmael@gmail.com>
2022-06-02 10:19:59 +0200

    qs

--- SAE/Code/src/modele/TestGraphe.java
+++ SAE/Code/src/modele/TestGraphe.java
@@ -1,5 +1,20 @@
 package modele;
+import donnee.*;
+import traitement.*;
+import java.sql.Date;
 
 public class TestGraphe {
+
+    public static void main(String[] args) {
+
+        // Test des classes graohes et sommets
+        Graphe g = new Graphe();
+        Sommet s1 = new Sommet(1, new Lieu(1, 1), new Date(1), new EspeceObservee(1, "a"));
+
+
+
+    }
+
+    
     
 }

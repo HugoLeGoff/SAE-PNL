2c7da5991634a862a17334be21d0ba82b9efe1b0
HugoLeGoff <hugo.legoff56@gmail.com>
2022-06-15 10:39:20 +0200

    up

--- SAE/Code/src/vue/AccueilController.java
+++ SAE/Code/src/vue/AccueilController.java
@@ -2,12 +2,13 @@
 package vue;
 import java.io.IOException;
 
-import data.*;
+import data.Database;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.fxml.*;
@@ -19,12 +20,23 @@ public class AccueilController {
 
 @FXML
     private Text actiontarget;
+
+@FXML 
+private TextField login ;
+
+@FXML
+private PasswordField pw ;
+
+
 @FXML
 
 
     protected void handleSubmitButtonAction(ActionEvent event) throws IOException{
+        boolean estValide = Database.testMDP(this.login.getText(), this.pw.getText());
         Scene scene = actiontarget.getScene();
         Parent root = FXMLLoader.load(getClass().getResource("Selection.fxml"));
         scene.setRoot(root);
     }
+
+
 }

4f2692206bfb9d6b8496d1cf5a515d4092b1652c
HugoLeGoff <hugo.legoff56@gmail.com>
2022-06-15 09:26:01 +0200

    up

---
+++ SAE/Code/src/vue/AccueilController.java
@@ -0,0 +1,28 @@
+package vue;
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.text.Text;
+import javafx.fxml.*;
+
+
+
+
+public class AccueilController {
+
+@FXML
+    private Text actiontarget;
+@FXML
+
+
+    protected void handleSubmitButtonAction(ActionEvent event) throws IOException{
+    Scene scene = actiontarget.getScene();
+    Parent root = FXMLLoader.load(getClass().getResource("Modele.fxml"));
+    scene.setRoot(root);
+       }
+}
--- SAE/Code/src/vue/Modele.java
+++ SAE/Code/src/vue/Modele.java
@@ -4,18 +4,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-public class Accueil extends Application {
+public class Modele extends Application {
 @Override
     public void start(Stage stage)
     throws Exception {
         
-        Parent root = FXMLLoader.load(getClass().getResource("Connexion.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Modele.fxml"));
         stage.setTitle("FXML Welcome");
         stage.setScene(new Scene(root, 300, 275));
         stage.show();
     }
 
     public static void main(String[] args) {
-        Application.launch(Accueil.class, args);
+        Application.launch(Modele.class, args);
     }
 }

5ec1bbe05fb7136d49f94ab7bbfd7ad421f15d9a
Litkaras <nolan.jametti@gmail.com>
2022-06-16 13:45:39 +0200

    up

--- SAE/Code/src/vue/HippoController.java
+++ SAE/Code/src/vue/HippoController.java
@@ -1,4 +1,3 @@
-
 package vue;
 import java.io.IOException;
 import java.sql.SQLException;
@@ -10,33 +9,67 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.text.Text;
 import javafx.fxml.*;
 
 
-
-
 public class HippoController {
 
-@FXML
-    private Text actiontarget;
+    @FXML
+    private Button ouvrirNewTuples;
 
-@FXML 
-    private TextField login ;
+    @FXML
+    private Button buttonAdd;
 
-@FXML
-    private PasswordField pw ;
+    @FXML
+    private TextField zoneObsH;
+
+    @FXML
+    private TextField zoneEspace;
+
+    @FXML
+    private TextField zoneSexe;
+
+    @FXML
+    private TextField zoneTemperatureEau;
+
+    @FXML
+    private TextField zoneTypeDePeche;
+
+    @FXML
+    private TextField zoneTaille;
+
+    @FXML
+    private TextField zoneGestant;
+        
+    
+    @FXML
 
 
-@FXML
+    protected void handleSubmitButtonAction(ActionEvent event) throws IOException{
+        if (event.getSource() == ouvrirNewTuples){
+            popUp();
+        } else if (event.getSource() == buttonAdd) {
+            ajouterTuple();
+        }
 
-
-    protected void handleSubmitButtonAction(ActionEvent event) throws IOException, SQLException{
-        Scene scene = actiontarget.getScene();
-        Parent root = FXMLLoader.load(getClass().getResource("Selection.fxml"));
-        scene.setRoot(root);
     }
 
+    protected void popUp () throws IOException{
+        Scene scene = ouvrirNewTuples.getScene();
+        Stage stage = new Stage(); 
+        Parent root = FXMLLoader.load(getClass().getResource("addHippo.fxml"));
+        scene.setRoot(root);
+        stage.setScene(scene);
+        stage.setTitle("Ajout de tuples");
+        stage.show();
+    }
 
+    protected void ajouterTuple () throws IOException{
+        zoneObsH.setText("test");
+    }
+    
 }

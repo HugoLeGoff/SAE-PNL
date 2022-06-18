4dc852df773bcd95dfef25e36add9d8bd3f8a120
HugoLeGoff <hugo.legoff56@gmail.com>
2022-06-18 21:00:20 +0200

    up

---
+++ SAE/Code/src/donnee/Table.java
@@ -0,0 +1,13 @@
+package donnee;
+
+public class Table {
+    private String table;
+
+    public Table(String newTable){
+        this.table = newTable;
+    }
+
+    public String getTable() {
+        return table;
+    }
+}

f1f0f75bcead6ffd8afbf3158c37095bdc36f0cc
enzofcna <enzolbd@gmail.com>
2022-06-20 17:22:28 +0200

    a

--- SAE/Code/src/data/ExportData.java
+++ SAE/Code/src/data/ExportData.java
@@ -33,23 +33,20 @@ public class ExportData {
         this.fileName = name;
 
         this.fileDirectory="C:\\Users\\public\\Downloads\\";//directory de base vers Utilisateur/public/telechargements
-
-
-
         
     }
     
     public String requete(){
         String ret="";
-        if(espece=="Hippocampes"){
+        if(espece.equals("Hippocampes")){
             ret="SELECT obsH,espece,sexe,temperatureEau,typePeche,taille,gestant,dateObs,heureObs,lieu_Lambert_X,lieu_Lambert_Y,idObservateur,nom,prenom FROM Obs_hippocampe LEFT JOIN Observation ON obsH = idObs LEFT JOIN Aobserve ON idObs = lobservation LEFT JOIN observateur ON lobservateur = idObservateur WHERE YEAR (dateObs) = "+annee+";";
-        }else if(espece=="GCI"){
+        }else if(espece.equals("GCI")){
             ret="SELECT obsG,nature,nombre,presentMaisNonObs,idNid,nomPlage,raisonArretObservation,nbEnvol,protection,bagueMale,bagueFemelle,dateObs,heureObs,lieu_Lambert_X,lieu_Lambert_Y,idObservateur,nom,prenom FROM obs_GCI LEFT JOIN Nid_GCI ON idNid=leNid LEFT JOIN Observation ON obsG = idObs LEFT JOIN Aobserve ON idObs = lobservation LEFT JOIN observateur ON lobservateur = idObservateur WHERE YEAR (dateObs) = "+annee+";";
-        }else if(espece=="Loutres"){
+        }else if(espece.equals("Loutres")){
             ret="SELECT obsL,commune,lieuDit,indice,idObs,dateObs,heureObs,lieu_Lambert_X,lieu_Lambert_Y,idObservateur,nom,prenom FROM obs_Loutre JOIN Observation ON obsL = idObs JOIN Aobserve ON idObs = lobservation LEFT JOIN observateur ON lobservateur = idObservateur WHERE YEAR (dateObs) = "+annee+";";
-        }else if(espece=="Batraciens"){
+        }else if(espece.equals("Batraciens")){
             ret="SELECT obsB,espece,nombreAdultes,nombreAmplexus,nombrePonte,nombreTetard,temperature,meteo_ciel,meteo_temp,meteo_vent,meteo_pluie,concerne_ZH,concernes_vege,dateObs,heureObs,lieu_Lambert_X,lieu_Lambert_Y,idObservateur,nom,prenom,zh_temporaire,zh_surface,zh_typeMare,zh_pente,zh_ouverture,natureVege,vegetation,decrit_LieuVege,idVegeLieu FROM Obs_Batracien JOIN Observation ON obsB = idObs JOIN Aobserve ON idObs = lobservation JOIN observateur ON lobservateur = idObservateur JOIN ZoneHumide ON concerne_ZH = zh_id JOIN Vegetation ON concernes_vege = idVege JOIN Lieu_Vegetation ON idVegeLieu = decrit_LieuVege WHERE YEAR (dateObs) = "+annee+";";
-        }else if(espece=="Chouettes"){
+        }else if(espece.equals("Chouettes")){
             ret="SELECT idObs,protocole,typeObs,leNumIndividu,numObs,numIndividu,espece,sexe,dateObs,heureObs,lieu_Lambert_X,lieu_Lambert_Y,idObservateur,nom,prenom FROM Obs_Chouette JOIN Chouette ON numIndividu=leNumIndividu JOIN Observation ON numObs=idObs JOIN Lieu ON (lieu_Lambert_X,lieu_Lambert_Y)=(coord_Lambert_X,coord_Lambert_Y) LEFT JOIN Aobserve ON idObs =lobservation LEFT JOIN observateur ON lobservateur = idObservateur WHERE YEAR (dateObs) = "+annee+";";
         }
         return ret;
--- SAE/Code/src/data/TestExportData.java
+++ SAE/Code/src/data/TestExportData.java
@@ -1,15 +1,12 @@
 package data;
 public class TestExportData{
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         ExportData test = new ExportData("testBatracien.csv","2016","Batracien");
-=======
-        ExportData test = new ExportData("test4.csv","2020","Batracien","C:\\Utilisateurs\\enzol\\Document\\");
->>>>>>> 984655898150158a34390fac54083d8b289e1e6c
         test.exportData();
         ExportData test2= new ExportData("testGCI.csv","2020","GCI");
         test2.exportData();
-        ExportData test3= new ExportData("testLoutre.csv","2017","Loutre");
+        ExportData test3= new ExportData("testLoutre.csv","2015","Loutre");
         test3.exportData();
         ExportData test4= new ExportData("testChouette.csv","2015","Chouette");
         test4.exportData();

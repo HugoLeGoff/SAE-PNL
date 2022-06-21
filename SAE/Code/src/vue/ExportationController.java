7443b082c667e8489cf29d1ad92e4f4e6692f0c2
enzofcna <enzolbd@gmail.com>
2022-06-21 22:24:31 +0200

    a

--- SAE/Code/src/vue/ExportationController.java
+++ SAE/Code/src/vue/ExportationController.java
@@ -7,6 +7,8 @@ import connexion.*;
 import donnee.*;
 import data.*;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -54,7 +56,7 @@ public class ExportationController {
     private Button deconnexion;
     
     @FXML 
-    private TextField annee;
+    private ComboBox<String> annee;
 
     @FXML 
     private TextField nomFichier;
@@ -63,6 +65,8 @@ public class ExportationController {
     @FXML private TableColumn<Table, String> tabColumn;
 
     @FXML private Label msgImport;
+
+    ObservableList<String> liste;
 
 
     @FXML
@@ -78,41 +82,62 @@ public class ExportationController {
         tables.setText("Tables "+nomEspece+" : ");
         
         
+        
 
         
         if(nomEspece.equals("Hippocampes")){
             tabColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("table"));
             AllData ad = new AllData();
             ArrayList<Table> tables = ad.tableHippo();
-            
+            liste = FXCollections.observableArrayList();
+            for(Table t :tables){
+                liste.add(t.getTable());
+            }
+            annee.setItems(liste);
             tableView.getItems().setAll(tables);
         }
         else if(nomEspece.equals("Batraciens")){
             tabColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("table"));
             AllData ad = new AllData();
             ArrayList<Table> tables = ad.tableBatraciens();
-            
+            liste = FXCollections.observableArrayList();
+            for(Table t :tables){
+                liste.add(t.getTable());
+            }
+            annee.setItems(liste);
             tableView.getItems().setAll(tables);
         }
         else if(nomEspece.equals("GCI")){
             tabColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("table"));
             AllData ad = new AllData();
             ArrayList<Table> tables = ad.tableGCI();
-            
+            liste = FXCollections.observableArrayList();
+            for(Table t :tables){
+                liste.add(t.getTable());
+            }
+            annee.setItems(liste);
             tableView.getItems().setAll(tables);
         }
         else if(nomEspece.equals("Chouettes")){
             tabColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("table"));
             AllData ad = new AllData();
             ArrayList<Table> tables = ad.tableChouettes();
-            
+            liste = FXCollections.observableArrayList();
+            for(Table t :tables){
+                liste.add(t.getTable());
+            }
+            annee.setItems(liste);
             tableView.getItems().setAll(tables);
         }
         else if(nomEspece.equals("Loutres")){
             tabColumn.setCellValueFactory(new PropertyValueFactory<Table, String>("table"));
             AllData ad = new AllData();
             ArrayList<Table> tables = ad.tableLoutres();
-            
+            liste = FXCollections.observableArrayList();
+            for(Table t :tables){
+                liste.add(t.getTable());
+            }
+            annee.setItems(liste);
             tableView.getItems().setAll(tables);
             }
         
@@ -129,14 +154,14 @@ public class ExportationController {
 
                 System.out.println(nomEspece);
 
-                ExportData fichier = new ExportData(this.nomFichier.getText(), this.annee.getText(), nomEspece);
+                ExportData fichier = new ExportData(this.nomFichier.getText(), this.annee.getValue(), nomEspece);
                 fichier.exportData();
 
                 msgImport.setText("Le fichier à été exporter à l'adresse \n C:/Users/Public/Downloads");
             
             }catch(NumberFormatException nfe){
                 msgImport.setText("Le numéro de l'année doit être valide");
-            }
+            } 
             
         }
 

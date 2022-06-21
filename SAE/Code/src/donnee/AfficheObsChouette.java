0dd24dc983a763832627631f6d9bd04273418c82
HugoLeGoff <hugo.legoff56@gmail.com>
2022-06-21 12:50:50 +0200

    up

--- SAE/Code/src/donnee/AfficheObsChouette.java
+++ SAE/Code/src/donnee/AfficheObsChouette.java
@@ -5,6 +5,7 @@ import java.sql.Time;
 
 public class AfficheObsChouette {
     
+    private String protocole;
     private String obsC;
     private String espece;
     private String sexe;
@@ -14,9 +15,11 @@ public class AfficheObsChouette {
     private String lieu_Lambert_X;
     private String lieu_Lambert_Y;
     private String idObservateur;
+    private String nom;
+    private String prenom;
 
 
-	public AfficheObsChouette(String obsC, String espece, String sexe, String dateObs, String heureObs, String lieu_Lambert_X, String lieu_Lambert_Y, String idObservateur) {
+	public AfficheObsChouette(String protocole, String typeObs, String leNumIndividu, String obsC, String espece, String sexe, String dateObs, String heureObs, String lieu_Lambert_X, String lieu_Lambert_Y, String idObservateur,String nom, String prenom) {
         
         this.obsC = obsC;
         this.espece = espece;
@@ -26,6 +29,8 @@ public class AfficheObsChouette {
         this.lieu_Lambert_X = lieu_Lambert_X;
         this.lieu_Lambert_Y = lieu_Lambert_Y;
         this.idObservateur = idObservateur;
+        this.nom = nom;
+        this.prenom = prenom;
 
 		
 	}

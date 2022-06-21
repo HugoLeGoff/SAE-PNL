b503315b685172c82b44cbd9a53adb06a960c792
Zedrum <yannmael.peron@gmail.com>
2022-06-21 15:50:15 +0200

    q

--- SAE/Code/src/vue/AddBatracienController.java
+++ SAE/Code/src/vue/AddBatracienController.java
@@ -8,11 +8,6 @@ import donnee.AfficheObsHippocampes;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import javafx.scene.text.Text;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -24,17 +19,31 @@ public class AddBatracienController {
 
     
     @FXML
-    private TextField zoneProtocole;
-    @FXML
-    private ComboBox<String> zoneTypeObs;
-    @FXML
-    private TextField zoneLeNumIndividu;
-    @FXML
-    private TextField zoneNumObs;
+    private TextField zoneObsB;
     @FXML
     private ComboBox<String> zoneEspece;
     @FXML
-    private ComboBox<String> zoneSexe;
+    private TextField zoneNombreAdultes;
+    @FXML
+    private TextField zoneNombreAmplexus;
+    @FXML
+    private TextField zoneNombrePonte;
+    @FXML
+    private TextField zoneNombreTetard;
+    @FXML
+    private TextField zoneTemperature;
+    @FXML
+    private TextField zoneMeteo_ciel;
+    @FXML
+    private TextField zoneMeteo_temp;
+    @FXML
+    private TextField zoneMeteo_vent;
+    @FXML
+    private TextField zoneMeteo_pluie;
+    @FXML
+    private TextField zoneConcerne_ZH;
+    @FXML
+    private TextField zoneConcernes_vege;
     @FXML
     private TextField zoneDateObs;
     @FXML
@@ -44,7 +53,26 @@ public class AddBatracienController {
     @FXML
     private TextField zoneLieu_Lambert_Y;
     @FXML
-    private TextField zoneIdObservation;
+    private TextField zoneLobservateur;
+    @FXML
+    private TextField zoneZh_temporaire;
+    @FXML
+    private TextField zoneZh_profondeur;
+    @FXML
+    private TextField zoneZh_surface;
+    @FXML
+    private TextField zoneZh_typeMare;
+    @FXML
+    private TextField zoneZh_pente;
+    @FXML
+    private TextField zoneZh_ouverture;
+    @FXML
+    private TextField zoneNatureVege;
+    @FXML
+    private TextField zoneIdVegeLieu;
+    @FXML
+    private TextField zoneVegetation;
+
     @FXML
     private Button buttonAdd;
 
@@ -55,31 +83,43 @@ public class AddBatracienController {
     
     @FXML
     private void initialize() throws SQLException {
-        liste = FXCollections.observableArrayList("SONORE","VISUELLE","SONORE_VISUELLE");
-        zoneTypeObs.setItems(liste);
-
-        liste = FXCollections.observableArrayList("EFFRAIE","HULOTTE","CHEVECHE");
+        liste = FXCollections.observableArrayList("CALAMITE","PELODYTE");
         zoneEspece.setItems(liste);
 
-        liste = FXCollections.observableArrayList("MALE","FEMELLE","INCONNU");
-        zoneSexe.setItems(liste);
+        AllData ad = new AllData();
+        zoneObsB.setText(ad.getID());
     }
 
     @FXML
     protected void handleSubmitButtonAction(ActionEvent event) throws IOException{
         if(event.getSource() == buttonAdd){
             HashMap<String,String> values = new HashMap<String,String>();
-            values.put("protocole",zoneProtocole.getText());
-            values.put("typeObs",zoneTypeObs.getValue());
-            values.put("leNumIndividu",zoneLeNumIndividu.getText());
-            values.put("numObs",zoneNumObs.getText());
+            values.put("obsB",zoneObsB.getText());
             values.put("espece",zoneEspece.getValue());
-            values.put("sexe",zoneSexe.getValue());
+            values.put("nombreAdultes",zoneNombreAdultes.getText());
+            values.put("nombreAmplexus",zoneNombreAmplexus.getText());
+            values.put("nombrePonte",zoneNombrePonte.getText());
+            values.put("nombreTetard",zoneNombreTetard.getText());
+            values.put("temperature",zoneTemperature.getText());
+            values.put("meteo_ciel",zoneMeteo_ciel.getText());
+            values.put("meteo_temp",zoneMeteo_temp.getText());
+            values.put("meteo_vent",zoneMeteo_vent.getText());
+            values.put("meteo_pluie",zoneMeteo_pluie.getText());
+            values.put("concerne_ZH",zoneConcerne_ZH.getText());
+            values.put("concernes_vege",zoneConcernes_vege.getText());
             values.put("dateObs",zoneDateObs.getText());
             values.put("heureObs",zoneHeureObs.getText());
             values.put("lieu_Lambert_X",zoneLieu_Lambert_X.getText());
             values.put("lieu_Lambert_Y",zoneLieu_Lambert_Y.getText());
-            values.put("idObservation",zoneIdObservation.getText());
+            values.put("lobservateur",zoneLobservateur.getText());
+            values.put("zh_temporaire",zoneZh_temporaire.getText());
+            values.put("zh_profondeur",zoneZh_profondeur.getText());
+            values.put("zh_surface",zoneZh_surface.getText());
+            values.put("zh_typeMare",zoneZh_typeMare.getText());
+            values.put("zh_pente",zoneZh_pente.getText());
+            values.put("zh_ouverture",zoneZh_ouverture.getText());
+            values.put("natureVege",zoneIdVegeLieu.getText());
+            values.put("idVegeLieu",zoneVegetation.getText());
 
 
             ChoixVal val = new ChoixVal("Chouettes", values);

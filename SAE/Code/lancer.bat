bb17dfe2573fcfaa6806dc31cf18a70ecd5b6e36
enzofcna <enzolbd@gmail.com>
2022-06-22 22:44:14 +0200

    a

--- SAE/Code/lancer.bat
+++ SAE/Code/lancer.bat
@@ -1,7 +1,6 @@
 cd ws
 javac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
 javac -encoding UTF8 -d ..\class ..\src\donnee\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
-javac -encoding UTF8 -d ..\class ..\src\vue\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
-javac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
-java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.web vue.Accueil
+javac -encoding UTF8 -d ..\class ..\src\vue\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.webjavac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
+java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.web vue.Accueil Picked up JAVA_TOOL_OPTIONS: -Dfile.encoding=UTF8
 pause

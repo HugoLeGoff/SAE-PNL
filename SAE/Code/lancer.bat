1310ce3c36095541f242ac5259a84270fbced767
HugoLeGoff <hugo.legoff56@gmail.com>
2022-06-23 10:42:19 +0200

    up

--- SAE/Code/lancer.bat
+++ SAE/Code/lancer.bat
@@ -2,4 +2,4 @@ cd ws
 javac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
 javac -encoding UTF8 -d ..\class ..\src\donnee\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
 javac -encoding UTF8 -d ..\class ..\src\vue\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.webjavac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
-java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.web vue.Accueil Picked up JAVA_TOOL_OPTIONS: -Dfile.encoding=UTF8
+java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.web vue.Accueil

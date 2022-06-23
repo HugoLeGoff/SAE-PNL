ed02fda737c3549c5dc8187d94b03d1c81d775ee
Zedrum <yannmael.peron@gmail.com>
2022-06-23 11:37:13 +0200

    q

--- SAE/Code/lancer.bat
+++ SAE/Code/lancer.bat
@@ -1,5 +1,6 @@
 cd ws
-javac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
 javac -encoding UTF8 -d ..\class ..\src\donnee\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
-javac -encoding UTF8 -d ..\class ..\src\vue\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.webjavac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
+javac -encoding UTF8 -d ..\class ..\src\carte\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.web
+javac -encoding UTF8 -d ..\class ..\src\data\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml
+javac -encoding UTF8 -d ..\class ..\src\vue\*.java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.web
 java --module-path ../lib/javafx-sdk-18.0.1/lib --add-modules javafx.base,javafx.controls,javafx.graphics,javafx.fxml,javafx.web vue.Accueil
